e8ca028daf57e51fca167b79b1f4db7ec5503f15
ramkrishna05 <108403796+ramkrishna05@users.noreply.github.com>
2023-10-10 19:17:24 +0530

    this is last commit

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,27 +1,35 @@
 package com.driver;
 
+// Order.java
 public class Order {
-
     private String id;
     private int deliveryTime;
 
     public Order(String id, String deliveryTime) {
-
-        // The deliveryTime has to converted from string to int and then stored in the attribute
-        //deliveryTime  = HH*60 + MM
-        this.id=id;
-        Integer time=settime(deliveryTime);
-        this.deliveryTime=time;
+        this.id = id;
+        Integer time = settime(deliveryTime); // Check if deliveryTime format is correct
+        this.deliveryTime = time;
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
-    public Integer settime(String deliveryTime)
-    {
-        String arr[]=deliveryTime.split(":");
-        return Integer.parseInt(arr[0])*60+Integer.parseInt(arr[1]);
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    // Helper method to convert HH:MM format delivery time to minutes
+    private int settime(String deliveryTime) {
+        String arr[] = deliveryTime.split(":");
+        if (arr.length != 2) {
+            throw new IllegalArgumentException("Invalid deliveryTime format. Use HH:MM.");
+        }
+        int hours = Integer.parseInt(arr[0]);
+        int minutes = Integer.parseInt(arr[1]);
+        if (hours < 0 || hours >= 24 || minutes < 0 || minutes >= 60) {
+            throw new IllegalArgumentException("Invalid deliveryTime values.");
+        }
+        return hours * 60 + minutes;
     }
 }
